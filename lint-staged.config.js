3502cd2793f0a0e836330c7506cb9a00e6f6375b
AutanaSoft <admin@autanasoft.com>
2024-01-13 05:10:49 -0400

    ci: update npm commands in Git hooks and lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.prod.json --noEmit',
-  '*.{js,jsx,ts,tsx}': ['pnpm lint:fix', 'jest --findRelatedTests --passWithNoTests'],
+  '*.{js,jsx,ts,tsx}': ['npm run lint:fix', 'jest --findRelatedTests --passWithNoTests'],
   '*.{md,json}': 'prettier --write',
 };
 
