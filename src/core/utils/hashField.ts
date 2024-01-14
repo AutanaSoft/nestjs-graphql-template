36234b2d9d6b82bad0c1ef3048386514c3274f70
AutanaSoft <admin@autanasoft.com>
2024-01-14 02:50:08 -0400

    ci: update package.json and husky scripts

--- src/core/utils/hashField.ts
+++ src/core/utils/hashField.ts
@@ -10,6 +10,12 @@ export const hashField = (password: string): string => {
   return hashSync(password, salt);
 };
 
+/**
+ * Verifies if a given field matches a hashed value.
+ * @param field - The field to be verified.
+ * @param hash - The hashed value to compare against.
+ * @returns True if the field matches the hash, false otherwise.
+ */
 export const verifyHashedField = (field: string, hash: string): boolean => {
   return compareSync(field, hash);
 };
