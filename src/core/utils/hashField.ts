4264f6e6e9e96a59188631f2387a3402912fd70f
AutanaSoft <admin@autanasoft.com>
2024-01-14 03:04:21 -0400

    ci: update package.json and husky scripts

--- src/core/utils/hashField.ts
+++ src/core/utils/hashField.ts
@@ -1,10 +1,5 @@
 import { compareSync, genSaltSync, hashSync } from 'bcrypt';
 
-/**
- * Hashes a password using bcrypt.
- * @param password - The password to be hashed.
- * @returns The hashed password.
- */
 export const hashField = (password: string): string => {
   const salt = genSaltSync(10);
   return hashSync(password, salt);
