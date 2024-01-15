e225e08e0dcde391352c4242cc55361e54ed3727
lCardenas <admin@autanasoft.com>
2024-01-15 07:33:14 -0400

    ci: update nyc.config.js to disable coverage check

--- nyc.config.js
+++ nyc.config.js
@@ -3,7 +3,7 @@ const MIN_COVERAGE = 80;
 const config = {
   all: true,
   exclude: ['src/main.ts', 'src/core/graphql/generated/**'],
-  'check-coverage': true,
+  'check-coverage': false,
   branches: MIN_COVERAGE,
   lines: MIN_COVERAGE,
   functions: MIN_COVERAGE,
