002825425f19dc634e7825e32a8ae489d5eafc65
AutanaSoft <admin@autanasoft.com>
2024-01-18 02:36:42 -0400

    ci: update Jest configuration and use Jest for unit and e2e tests

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ const config = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.prod.json --noEmit',
   '*.{js,jsx,ts,tsx}': [
     'pnpm lint:fix',
-    'ts-jest --findRelatedTests --passWithNoTests',
+    'jest --findRelatedTests --passWithNoTests',
   ],
   '*.{md,json}': 'prettier --write',
 }
