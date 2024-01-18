289095b42c11882a4b0c6bde32b281970daf9fd4
AutanaSoft <admin@autanasoft.com>
2024-01-18 02:16:31 -0400

    ci: update ts-jest version and add as a dev dependency

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ const config = {
   '**/*.ts?(x)': () => 'tsc -p tsconfig.prod.json --noEmit',
   '*.{js,jsx,ts,tsx}': [
     'pnpm lint:fix',
-    'jest --findRelatedTests --passWithNoTests',
+    'ts-jest --findRelatedTests --passWithNoTests',
   ],
   '*.{md,json}': 'prettier --write',
 }
